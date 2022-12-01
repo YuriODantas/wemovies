2d41ceb479101cace7f2f3ad0ab941fe02e77fcf
YuriODantas <iruy3343@gmail.com>
2022-12-01 15:26:22 -0300

    create page notFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,16 @@
 import { FC } from 'react'
+import ButtonBack from '../components/ButtonBack'
+import ContainerMessage from '../components/ContainerMessage'
+import Image from '../components/Image'
+import Message from '../components/Message'
 
 const NotFound: FC = () => {
   return (
-    <div>Página não encontrada</div>
+    <ContainerMessage>
+      <Message>Parece que não há nada por aqui :(</Message>
+      <Image src="/icons/CartEmpty.svg" alt="Carrinho vazio" />
+      <ButtonBack />
+    </ContainerMessage>
   )
 }
 
